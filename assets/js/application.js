57a4b13925c66ceaa780a216f8171115f2dc756b
Patrick Sobolewski <Patrick Sobolewski>
2016-03-14 22:58:58 -0600

    Added the main list to the sidebar.

---
+++ assets/js/application.js
@@ -0,0 +1,48 @@
+$(document).ready(function() {
+
+    // Handles the hidden blog on the main page
+    //$(".blog-button").on("click", function() {
+    //    $(".content").removeClass("hidden");
+    //});
+
+    // TODO: Add the search stuff (or just change it)
+    /*
+
+    // Save search form
+    $("#nav-search").keydown(function(e) {
+	    if(e.which == 13) {
+	    	var searchVal = $('#nav-search').val();
+       		sessionStorage.setItem("searchVal", searchVal);
+        }
+	});
+
+    // Write search term to search form
+	$('#search-field').val(sessionStorage.getItem("searchVal"));
+    */
+
+
+    // TODO: add the project list files
+    /*
+
+    var sliding = false;
+
+    // Fancy animation on the project entries
+    $(".projectList").on("mouseenter mouseleave", ".project-entry", function() {
+        $(this).closest('.project-entry').find('.project-buttons').stop();
+        $(this).closest('.project-entry').find('.project-buttons').slideToggle(250);
+    });
+
+    // Outdated.
+    //$(".tag-projects").on('mouseenter', 'article', function() {
+    //    $(this).closest('.tag-projects').find('.project-buttons').stop();
+    //    $(this).closest('.tag-projects').find('.project-buttons').slideDown(250);
+    //});
+    //$(".tag-projects").on('mouseleave', 'article', function() {
+    //    $(this).closest('.tag-projects').find('.project-buttons').stop();
+    //    $(this).closest('.tag-projects').find('.project-buttons').slideUp(250);
+    //});
+
+    */
+
+
+});
